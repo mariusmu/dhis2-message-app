162e4ec7032fd9e7dfeae5e426497f0d73edc699
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-07 21:49:12 +0100

    Added some basic url constants to use

---
+++ app/Constants/UrlConstants.js
@@ -0,0 +1,5 @@
+module.exports = {
+    baseUrl: "http://localhost:8082/",
+    loginGrantUrl :  "http://localhost:8082/api/oAuth2Clients",
+    loginUrl : "http://localhost:8082/uaa/oauth/token"
+}
