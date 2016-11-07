7aa39d119e7897698c564649c5b6319c30b91494
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-07 14:35:16 +0100

    added redux middleware

--- app/app.jsx
+++ app/app.jsx
@@ -2,17 +2,39 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Router, Link, Route, browserHistory} from 'react-router';
 
-import Start from './Components/Start'
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux' 
+import MessageReducer from './Reducers/message.reducer';
+
+import StartComponent from './Components/Start'
 import Next from './Components/Next';
+
+function mapStateToProps (state,props) {
+    return {
+        id: props.params.id,
+        filter: props.location.query.filter
+    };
+}
+
+const store = createStore(combineReducers({
+    start : MessageReducer,
+    routing: routerReducer
+}));
+
+const history = syncHistoryWithStore(browserHistory, store);
+
 
 /**
  * Route different urls to different Components
  */
 ReactDom.render((
-    <Router history={browserHistory}>
-        <Route path="/" component={Start}/>
+   <Provider store={store}>
+    <Router history={history}>
+        <Route path="/" component={StartComponent}/>
         <Route path="next" component={Next}/>
     </Router>
+    </Provider>
 ), document.getElementById("app"));
 
 /**
@@ -20,4 +42,6 @@ ReactDom.render((
  */
 if(module.hot) {
     module.hot.accept();
-}  
+};
+
+require("./Style/main.less");
