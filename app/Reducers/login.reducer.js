c060bd1ece312b99bb87426eb34f2bd6129c3337
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-08 20:40:40 +0100

    Added object with correct reducer functionality for login

--- app/Reducers/login.reducer.js
+++ app/Reducers/login.reducer.js
@@ -4,11 +4,27 @@ import ActionConstants from 'constants/ActionConstants';
 module.exports = function(state = [], action) {
     switch(action.type) {
         case ActionConstants.OAUTH_CLIENT_CREATED :
-            return action.token;   
-        case ActionConstants.OAUTH_CLIENT_ERROR :
-            return "err";
-        default:
-        return "Not added";
+            return Object.assign({}, state, 
+            {
+                oAuthStatus: "OK",
+                oAuthToken: action.token,
+                oAuthError: []
+            });   
 
-    }
+        case ActionConstants.OAUTH_CLIENT_ERROR :
+            return Object.assign({}, state, 
+            {
+                oAuthStatus: "NOTOK",
+                oAuthToken: null, 
+                oAuthError: action.error
+            });   
+
+        default:
+            return Object.assign({}, state, 
+            {
+                oAuthStatus: "NOTOK",
+                oAuthToken: null, 
+                oAuthError: []
+            });
+        }
 }
