ba0bc57675ded7cf210565eff47b4d7714ee3c3c
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-09 17:40:46 +0100

    Removed oAuth logic, and added login function

--- app/Actions/login.action.js
+++ app/Actions/login.action.js
@@ -9,50 +9,20 @@ function isNull(variable) {
     return false;
 }
 
-const options = {
-    method : "POST",
-    headers: {
-        "Content-Type" : 'application/json'
-    },
-    json: true
-}
 
-export const checkForOAuth = () => dispatch => {
-    const token = PersistanceService.getClientToken();
-    if (token) dispatch({type: ActionConstants.OAUTH_CLIENT_CREATED, token :token});
-}
-
-export const createOAuthClient = (username, password, cid, name) => dispatch => {
-    const b64 = new Buffer(username + ":"+password).toString("base64");
-    const oAuthObj = {
-        name: name,
-        cid : cid,
-        secret: SecretConstants.OAUTH_SECRET,
-           "grantTypes" : [
-                "password",
-                 "refresh_token",
-                "authorization_code"
-        ],
-        redirectUris: [
-        ]
+export const loginUser = (username, password, cid) => dispatch => {
+    const b64 = new Buffer(cid + ":"+SecretConstants.OAUTH_SECRET).toString("base64");
+    const formObj = {
+        grant_type: "password",
+        username: username,
+        password: password
     };
-    ApiService.unauthenticatedPost(oAuthObj, UrlConstants.loginGrantUrl, b64)
+    console.log(username, password, cid);
+    ApiService.unauthenticatedFormPost(formObj, UrlConstants.loginUrl, b64)
         .then(res => {
             console.log(res);
-            const token = res.response.uid;
-            PersistanceService.persistClientToken(token);
-            dispatch({type: ActionConstants.OAUTH_CLIENT_CREATED, token: token})})
+        })
         .catch(err => {
-            dispatch({type: ActionConstants.OAUTH_CLIENT_ERROR, error: err.response.body.response.errorReports})
+           console.log(err);
     });
-
-}
-
-export const logOut = () => dispatch => {
-    PersistanceService.deleteClientToken();
-    dispatch({type: ActionConstants.OAUTH_CLIENT_DELETED})
-}
-
-export function logIn(username, password) {
-    return request()
 }
