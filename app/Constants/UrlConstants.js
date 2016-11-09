5bdd6c3fa813ba0da085442c5a9194d5b306ea0b
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-09 17:47:47 +0100

    Added some more constants

--- app/Constants/UrlConstants.js
+++ app/Constants/UrlConstants.js
@@ -1,5 +1,7 @@
 module.exports = {
     baseUrl: "http://localhost:8082/",
     loginGrantUrl :  "http://localhost:8082/api/oAuth2Clients",
-    loginUrl : "http://localhost:8082/uaa/oauth/token"
+    loginUrl : "http://localhost:8082/uaa/oauth/token",
+    messagesUrl: "http://localhost:8082/api/messageConversations",
+    userQuery: "http://localhost:8082/api/users",
 }
