db2eea2b97ca6bde85892392f969d9e688e6c090
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-09 17:49:17 +0100

    Added uAuthenticated get and changed createOption to support form-encode

--- app/Services/ApiService.js
+++ app/Services/ApiService.js
@@ -6,17 +6,20 @@ class ApiService {
      * Create option object for request
      * @param uri the uri to send request to
      * @param method the method to use in the request
+     * @param json boolean
      */
-    createOptions(uri, method) {
+    createOptions(uri, method, json) {
+        const contentType = json === true ? 'application/json' : 'application/x-www-form-urlencoded'
         return {
             method: method,
             uri: uri,
             headers: {
-                "Content-Type" : "application/json"
+                "Content-Type" : contentType
             },
-            json: true
+            json: json
         }
     }
+
 
     /**
      * POST to Api as an unauthenticated user
@@ -25,8 +28,21 @@ class ApiService {
      * @param b64 base64 authentication token
      */
     unauthenticatedPost(json, uri, b64) {
-        let options = this.createOptions(uri, "POST");
+        let options = this.createOptions(uri, "POST", true);
         options.body = json;
+        options.headers.Authorization = "Basic " + b64;
+        return req(options);
+    };
+
+    /**
+     * POST to Api as an unauthenticated user
+     * @param form object the form object to post
+     * @param uri the uri to post to
+     * @param b64 base64 authentication token
+     */
+    unauthenticatedFormPost(form, uri, b64) {
+        let options = this.createOptions(uri, "POST", false);
+        options.form = form;
         options.headers.Authorization = "Basic " + b64;
         return req(options);
     };
@@ -38,9 +54,29 @@ class ApiService {
      * @param token the refresh token
      */
     authenticatedPost(json, uri, token) {
-        let options = this.createOptions(uri, "POST");
+        let options = this.createOptions(uri, "POST", true);
         options.headers.token = token;
         options.body = JSON.stringify(json);
+        return req(options);
+    }
+
+    /**
+     * GET request to api as an unauthenticated user
+     * @param uri the url to the endpoint
+     */
+    unauthenticatedGet(uri) {
+        let options = this.createOptions(uri, "GET", false);
+        return req(options);
+    }
+
+       /**
+     * GET request to api as an unauthenticated user
+     * @param uri the url to the endpoint
+     */
+    authenticatedGet(uri, token) {
+        let options = this.createOptions(uri, "GET", false);
+        options.headers.Authorization = "Bearer " + token;
+
         return req(options);
     }
 }
