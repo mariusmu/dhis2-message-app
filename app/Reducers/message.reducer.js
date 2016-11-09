0067a717a6f95859e4c8e89f2ef2682ea3a35495
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-09 17:48:10 +0100

    added reducer functionality for messages

--- app/Reducers/message.reducer.js
+++ app/Reducers/message.reducer.js
@@ -1,11 +1,37 @@
+import ActionConstants from 'constants/ActionConstants';
 import InitialState from './InitialState';
 
-module.exports = function(state = InitialState, action) {
-    switch(action.type) {
-        case 'START' :
-            return action.start;     
+module.exports = function (state = [], action) {
+    switch (action.type) {
+        case ActionConstants.FETCH_MESSAGES_OK:
+            return Object.assign({}, state, {
+                pager: action.pager
+            });
+
+        case ActionConstants.FETCH_ONE_MESSAGE_OK:
+            if (action.message.id) {
+                return Object.assign({}, state, {
+                    messages: state.messages.concat(action.message)
+                });
+            }
+
+        case ActionConstants.SELECT_MESSAGE:
+            if (action.id) {
+                return Object.assign({}, state, {
+                    selected_message: action.id
+                });
+            }
+        case ActionConstants.UNSELECT_MESSAGE:
+            return Object.assign({}, state, {
+                selected_message: null
+            });
         default:
-        return state;
+            return Object.assign({}, state, {
+                messages: [],
+                messages_error: [],
+                pager: null,
+                selected_message: null
+            });
 
     }
 }
