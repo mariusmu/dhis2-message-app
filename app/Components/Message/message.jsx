72619a404c9ee98974cae67316d4fc9d56bb4b5e
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-10 20:55:56 +0100

    Added user reducer

--- app/Components/Message/message.jsx
+++ app/Components/Message/message.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
 import {fetchAllMessages, selectMessage} from 'actions/message.action';
+import {fetchAllUsers} from 'actions/user.action';
 import ErrorDisplay from 'components/common/error-display';
 import AllMessages from 'components/message/AllMessages/all-messages';
 import ReadMessage from 'components/message/ReadMessage/read-message';
@@ -14,13 +15,18 @@ class Message extends React.Component {
         this.state = {};
         this.routeState = "INIT";
         this.selectMessage = this.selectMessage.bind(this);
+        this.state.selectedMessage = null;
         
     }
 
     componentDidMount() {
         fetchAllMessages()(this.props.dispatch);
+       fetchAllUsers()(this.props.dispatch);
     }
 
+    componentWillReceiveProps(props) {
+        console.log(props);
+    }
 
 
     render() {
@@ -33,8 +39,8 @@ class Message extends React.Component {
 
         return (
             this.props.selectedMessage == null ? 
-                <AllMessages messages={this.props.messages} selectMessage={this.selectMessage} dispatch={this.props.dispatch}/> : 
-                <ReadMessage message={this.props.messages.filter(
+                <AllMessages users={this.props.users} messages={this.props.messages} selectMessage={this.selectMessage} dispatch={this.props.dispatch}/> : 
+                <ReadMessage users={this.props.users} message={this.props.messages.filter(
                     (val) => {
                         return val.id == this.props.selectedMessage})[0]} dispatch={this.props.dispatch}/>
         )
@@ -53,12 +59,12 @@ class Message extends React.Component {
 }
 
 function mapStateToProps(state, ownprops) {
-    
+    console.log(state);
     return {
         messages: state.messages.messages,
         messages_error: state.messages.messages_error,
-        selectedMessage: state.messages.selected_message
-        
+        selectedMessage: state.messages.selected_message,
+        users: state.users.users
     };
 }
 
