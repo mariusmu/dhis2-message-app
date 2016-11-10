363e2ff89efa9b0a982e46c6e602c1e69e6fa7ff
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-10 20:58:00 +0100

    Added reduce functionality for conversation retrival

--- app/Reducers/message.reducer.js
+++ app/Reducers/message.reducer.js
@@ -1,17 +1,34 @@
 import ActionConstants from 'constants/ActionConstants';
 import InitialState from './InitialState';
 
-module.exports = function (state = [], action) {
+module.exports = function (state = InitialState.messages, action) {
     switch (action.type) {
         case ActionConstants.FETCH_MESSAGES_OK:
+            console.log("Messages are fetched");
             return Object.assign({}, state, {
-                pager: action.pager
+                pager: action.pager,
+                messages: action.messages
             });
 
         case ActionConstants.FETCH_ONE_MESSAGE_OK:
             if (action.message.id) {
                 return Object.assign({}, state, {
                     messages: state.messages.concat(action.message)
+                });
+            }
+
+        case ActionConstants.FETCH_CONVERSATIONS_OK:
+            if (action.messages) {
+                let messageArr = state.messages.slice(0);
+                
+                /** Find the correct message in the state message array 
+                 * Then assign a conversation field with 
+                 */
+                messageArr.filter(val => { return val.id == action.messageId})
+                    [0].conversations = action.messages;
+                    
+                return Object.assign({}, state, {
+                    messages: messageArr
                 });
             }
 
@@ -26,12 +43,7 @@ module.exports = function (state = [], action) {
                 selected_message: null
             });
         default:
-            return Object.assign({}, state, {
-                messages: [],
-                messages_error: [],
-                pager: null,
-                selected_message: null
-            });
+            return state;
 
     }
 }
