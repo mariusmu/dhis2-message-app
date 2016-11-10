0c75526c5bf3299494657c4d97385ae475ed891a
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-10 20:50:13 +0100

    Removed uneccessary pr message calls. Added getConversations

--- app/Actions/message.action.js
+++ app/Actions/message.action.js
@@ -4,46 +4,51 @@ import UrlConstants from 'constants/UrlConstants';
 import ActionConstants from 'constants/ActionConstants';
 import async from 'async';
 
-const getUsers = (message, token) => dispatch => {
-    console.log("In get users");
-    return ApiService.authenticatedGet(UrlConstants.userQuery + "/" + message.lastSender.id, token)
-        .then(res => {
-            message.lastSenderName = JSON.parse(res).displayName;
-            dispatch({ type: ActionConstants.FETCH_ONE_MESSAGE_OK, message: message });
-            return null;
-        })
-        .catch(lastUserResErr => { throw lastUserResErr });
-}
+const token = "7b4df143-52bf-4cab-b0ef-38ba94d30e2f";
+export const fetchAllMessages = () => dispatch => {
 
-const getMessageRest = (messages, token) => dispatch => {
-    return messages.messageConversations.map((val, key) => {
-        ApiService.authenticatedGet(UrlConstants.messagesUrl + "/" + val.id, token)
-            .then(singleRes => {
-                let singleMessage = JSON.parse(singleRes);
-                getUsers(singleMessage, token)(dispatch);
-                return null;
-            })
-            .catch(singleResErr => { throw singleResErr });
-    });
-}
-
-
-export const fetchAllMessages = (token) => dispatch => {
-    const token = "4551a0de-785e-4715-a3d5-f502c310715f";
-    return ApiService.authenticatedGet(UrlConstants.messagesUrl, token)
+    return ApiService.authenticatedGet(UrlConstants.messagesUrl + "?fields=:all", token)
         .then(res => {
             const parsedBody = JSON.parse(res);
-            dispatch({ type: ActionConstants.FETCH_MESSAGES_OK, pager: parsedBody.pager });
-            getMessageRest(parsedBody, token)(dispatch);
+            dispatch(
+                { 
+                    type: ActionConstants.FETCH_MESSAGES_OK, 
+                    pager: parsedBody.pager, 
+                    messages: parsedBody.messageConversations 
+                });
+            
             return null;
         })
         .catch(err => { throw err; });
 }
 
+export const fetchAllConversations = (messageId) => dispatch => {
+    return ApiService.authenticatedGet(UrlConstants.messagesUrl + "/" + messageId + "/messages", token)
+        .then(res => {
+            const parsedBody = JSON.parse(res);
+            if(parsedBody) {
+                dispatch (
+                    {
+                        messageId: messageId,
+                        type: ActionConstants.FETCH_CONVERSATIONS_OK, 
+                        messages: parsedBody.messages
+                    });
+            }
+        })
+        .catch(err => { throw err; });
+}
+
 export const selectMessage = (messageId) => dispatch => {
-    dispatch({type: ActionConstants.SELECT_MESSAGE, id: messageId});
+    dispatch(
+        {
+            type: ActionConstants.SELECT_MESSAGE, 
+            id: messageId
+        });
 }
 
 export const unSelectMessage = () => dispatch => {
-    dispatch({type: ActionConstants.UNSELECT_MESSAGE});
+    dispatch(
+        {
+            type: ActionConstants.UNSELECT_MESSAGE
+        });
 }
