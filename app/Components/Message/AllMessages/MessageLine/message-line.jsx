a43cc7176f9d61deaf255b16ac455a55f09a8024
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-10 20:51:22 +0100

    Changed which parameters to display in the component

--- app/Components/Message/AllMessages/MessageLine/message-line.jsx
+++ app/Components/Message/AllMessages/MessageLine/message-line.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
 class MessageLine extends React.Component {
-    constructor() {
+    constructor(props) {
         super();
         this.selectMessage = this.selectMessage.bind(this);
+        this.generateUsers(props);
+        
     }
+
+    
+
+    generateUsers(props) {
+     
+    }
+
     render() {
 
         return(
          <tr>
-            <td>{this.props.message.lastSenderName}</td>
+            <td>{this.props.message.lastSenderFirstname} {this.props.message.lastSenderSurname}</td>
             <td><a onClick={this.selectMessage}>{this.props.message.subject}</a></td>
             <td>{this.props.message.lastUpdated.substr(0, 10)}</td>
          </tr>
