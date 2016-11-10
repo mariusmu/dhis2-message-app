2faaaf13ffa49f3623b5a50e03ae11951d36c450
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-10 20:58:34 +0100

    Added user reducer to combineReducers

--- app/Reducers/root.reducer.js
+++ app/Reducers/root.reducer.js
@@ -4,10 +4,12 @@ import {routerReducer} from 'react-router-redux';
 import MessageReducer from './message.reducer'
 import LoginReducer from './login.reducer';
 import oAuthReducer from './oauth.reducer';
+import userReducer from './user.reducer';
 
 export var rootReducer = combineReducers({
         oAuthClient : oAuthReducer,
         login: LoginReducer,
         routing: routerReducer,
-        messages: MessageReducer
+        messages: MessageReducer,
+        users: userReducer
 });
