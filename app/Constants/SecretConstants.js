dc23ec10d4a235c830bd443af06db32f7886d37c
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-17 14:00:23 +0100

    Added Dropbox access constants

--- app/Constants/SecretConstants.js
+++ app/Constants/SecretConstants.js
@@ -1,5 +1,7 @@
 module.exports = {
     OAUTH_SECRET : "1e6db50c-0fee-11e5-98d0-3c15c2c6caf5",
     OAUTH_NAME : "Developement token",
-    OAUTH_CID : "dev"
+    OAUTH_CID : "dev",
+    DROPBOX_APP_ID: "a9zjeg1od1w23a7",
+    DROPBOX_ACCESS_TOKEN : "AlphCtYeaJAAAAAAAAAACELytb6cqNJOyNJYgoRnBPP1ziJ6Rn1FsTv7K9mBqO-N"
 }
