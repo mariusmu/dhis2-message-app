dbdf6925c71f5b9e7fa28583b128261f2033b5f4
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-17 13:57:14 +0100

    Modified to use fetch api json

--- app/Actions/message.action.js
+++ app/Actions/message.action.js
@@ -4,44 +4,90 @@ import UrlConstants from 'constants/UrlConstants';
 import ActionConstants from 'constants/ActionConstants';
 import async from 'async';
 
-const token = "7b4df143-52bf-4cab-b0ef-38ba94d30e2f";
+
+const token = null;
+
+export const fetchOneMessage = (messageId) => dispatch => {
+    return ApiService.authenticatedGet(UrlConstants.messagesUrl + "/" + messageId, token)
+        .then(res => {
+            console.log(res);
+            res.json().then(parsedBody => {
+                if (parsedBody) {
+                    dispatch(
+                        {
+                            type: ActionConstants.FETCH_ONE_MESSAGE_OK,
+                            message: parsedBody
+                        });
+                }
+            });
+        })
+        .catch(err => { throw err; });
+};
 export const fetchAllMessages = () => dispatch => {
 
     return ApiService.authenticatedGet(UrlConstants.messagesUrl + "?fields=:all", token)
         .then(res => {
-            const parsedBody = JSON.parse(res);
-            dispatch(
-                { 
-                    type: ActionConstants.FETCH_MESSAGES_OK, 
-                    pager: parsedBody.pager, 
-                    messages: parsedBody.messageConversations 
-                });
-            
-            return null;
+            res.json().then(parsedBody => {
+                dispatch(
+                    {
+                        type: ActionConstants.FETCH_MESSAGES_OK,
+                        pager: parsedBody.pager,
+                        messages: parsedBody.messageConversations
+                    });
+
+                return null;
+            })
         })
-        .catch(err => { throw err; });
+        .catch(err => { console.log(err.message); });
 }
 
 export const fetchAllConversations = (messageId) => dispatch => {
     return ApiService.authenticatedGet(UrlConstants.messagesUrl + "/" + messageId + "/messages", token)
         .then(res => {
-            const parsedBody = JSON.parse(res);
-            if(parsedBody) {
-                dispatch (
-                    {
-                        messageId: messageId,
-                        type: ActionConstants.FETCH_CONVERSATIONS_OK, 
-                        messages: parsedBody.messages
-                    });
-            }
+            res.json().then(parsedBody => {
+                if (parsedBody) {
+                    dispatch(
+                        {
+                            messageId: messageId,
+                            type: ActionConstants.FETCH_CONVERSATIONS_OK,
+                            messages: parsedBody.messages,
+                            fullMessage: parsedBody
+                        });
+                }
+            })
         })
         .catch(err => { throw err; });
+}
+
+
+export const postConversation = (messageId, message, internal, attachements) => dispatch => {
+    const messageObj = {
+        text: message,
+        attachements: attachements
+    };
+    const stringified = JSON.stringify(messageObj);
+    const baseUrl = UrlConstants.messagesUrl + "/" + messageId;
+    const uri = internal ? baseUrl+"?internal=true" : baseUrl;
+
+    return ApiService.authenticatedPost(stringified, uri, token)
+        .then(res => {
+            if (res.status != 201) {
+                return dispatch({ type: ActionConstants.CONVERSATION_POST_ERROR, error: res.statusCode + " when posting conversation" });
+            }
+            dispatch({ type: ActionConstants.CONVERSATION_POST_OK });
+        })
+        .catch(err => {
+            console.log(err);
+            //TODO: Json parse error
+            dispatch({ type: ActionConstants.CONVERSATION_POST_ERROR, error: err });
+        });
+
 }
 
 export const selectMessage = (messageId) => dispatch => {
     dispatch(
         {
-            type: ActionConstants.SELECT_MESSAGE, 
+            type: ActionConstants.SELECT_MESSAGE,
             id: messageId
         });
 }
