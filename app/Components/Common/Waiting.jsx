f0ac4fb3490bf8fbe601a608b172a70ca95ed23d
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-17 14:05:34 +0100

    Adde spinning bar

---
+++ app/Components/Common/Waiting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Waiting extends React.Component {
+    render() {
+        return(
+            <div className="spinround">
+            </div>
+        );
+    }
+}
+
+module.exports = Waiting;
