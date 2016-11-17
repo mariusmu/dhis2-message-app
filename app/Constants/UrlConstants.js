564be41b586ee546e7c78f28e231c707bf0cdc26
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-17 14:00:51 +0100

    Added dropbox api urls

--- app/Constants/UrlConstants.js
+++ app/Constants/UrlConstants.js
@@ -4,4 +4,7 @@ module.exports = {
     loginUrl : "http://localhost:8082/uaa/oauth/token",
     messagesUrl: "http://localhost:8082/api/messageConversations",
     userQuery: "http://localhost:8082/api/users",
+    fileUrl: "http://localhost:8082/api/fileResources",
+    DROPBOX_DELETE_FILE: 'https://api.dropboxapi.com/1/fileops/delete',
+    DROPBOX_READ_FILE: 'https://content.dropboxapi.com/1/files/auto/'
 }
