bbe8d4830e4810bba5b88b5bc7c5d78269bc9fdd
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-17 14:02:25 +0100

    Removed unused reducers

--- app/Reducers/root.reducer.js
+++ app/Reducers/root.reducer.js
@@ -1,15 +1,9 @@
 import {combineReducers} from 'redux';
-import {routerReducer} from 'react-router-redux';
 
 import MessageReducer from './message.reducer'
-import LoginReducer from './login.reducer';
-import oAuthReducer from './oauth.reducer';
 import userReducer from './user.reducer';
 
 export var rootReducer = combineReducers({
-        oAuthClient : oAuthReducer,
-        login: LoginReducer,
-        routing: routerReducer,
         messages: MessageReducer,
         users: userReducer
 });
