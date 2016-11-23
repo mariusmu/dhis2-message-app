772d545bc781f2aa8b02ad2eae697a921dbb20bf
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-23 15:51:22 +0100

    Added support for local webserver

--- app/Actions/file.action.js
+++ app/Actions/file.action.js
@@ -7,20 +7,33 @@ import Dropbox from 'dropbox';
 import uuid from 'uuid';
 import fs from 'fs';
 
-export const uploadFile = (file) => {
-    const dropbox = new Dropbox({ accessToken: SecretConstants.DROPBOX_ACCESS_TOKEN });
-    return dropbox.filesUpload({ path: "/" + uuid.v4() + "/" + file.name, contents: file });
+export const uploadFile = (files) => {
+    return new Promise((resolve, reject) => {
+        const xhr =  new XMLHttpRequest();
+        var formData = new FormData();
+        formData.append("files", files);
+        xhr.open("POST", UrlConstants.FILE_SERVER + "file/upload");
+        xhr.onload = (res) => {
+            if(res.target.status != 201) reject(xhr.responseText);
+            resolve(JSON.parse(xhr.responseText)); 
+        }
+        
+        xhr.send(formData);
+    });
+    //const dropbox = new Dropbox({ accessToken: SecretConstants.DROPBOX_ACCESS_TOKEN });
+    //return dropbox.filesUpload({ path: "/" + uuid.v4() + "/" + file.name, contents: file });
 };
 
 export const deleteFile = (path) => {
-    const parentPath = path.substr(1, 36);
-    const form = "root=sandbox&path="+parentPath;
-    return ApiService.authenticatedFormPost(form, UrlConstants.DROPBOX_DELETE_FILE, SecretConstants.DROPBOX_ACCESS_TOKEN);
+    //const parentPath = path.substr(1, 36);
+    //const form = "root=sandbox&path="+parentPath;
+    //return ApiService.authenticatedFormPost(form, UrlConstants.DROPBOX_DELETE_FILE, SecretConstants.DROPBOX_ACCESS_TOKEN);
+    return ApiService.unauthenticatedDelete(UrlConstants.FILE_SERVER + "file/delete/" + path);
 }
 
 export const readFile = (path) => {
     return new Promise((resolve, reject) => {
-        ApiService.authenticatedGet(UrlConstants.DROPBOX_READ_FILE + path, SecretConstants.DROPBOX_ACCESS_TOKEN)
+        ApiService.authenticatedGet(UrlConstants.FILE_SERVER + path, SecretConstants.DROPBOX_ACCESS_TOKEN)
         .then(res => {
             res.blob()
                 .then(textRes => resolve(textRes))
