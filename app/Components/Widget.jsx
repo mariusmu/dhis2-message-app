04e474f54391130f967bb60fe908e8ca9b88ef9f
ophelie <ophelie.andre@live.fr>
2016-11-23 20:18:51 +0100

    graphs preview

--- app/Components/Widget.jsx
+++ app/Components/Widget.jsx
@@ -3,14 +3,14 @@
  */
 
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Image, Modal, Row, Button } from 'react-bootstrap';
 import ShareButton from './ShareButton';
 
 
 class Widget extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { source:'',type:this.props.type};
+        this.state = { source:'',type:this.props.type, showModal: false};
     }
 
 
@@ -20,7 +20,7 @@ class Widget extends React.Component {
             <div className="widgetImg">
                 <div className="panel panel-default">
                     <div className="panel-heading">
-                        <i className="fa fa-bar-chart-o fa-fw"></i> {this.props.name}
+                        <i className="fa fa-bar-chart-o fa-fw"/> {this.props.name}
                         <div className="pull-right">
                             <div className="btn-group">
                                 <ShareButton id={this.props.id} type={this.state.type}/>
@@ -28,14 +28,39 @@ class Widget extends React.Component {
                         </div>
                     </div>
                     <div className="panel-body">
-                        <Image className="widgetImgTest" src={source} rounded/>
+                        <Image className="widgetImgTest" src={source} rounded onClick={this._previewImage.bind(this)}/>
                     </div>
+                    <Modal show={this.state.showModal} onHide={this._close.bind(this)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Preview</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Row bsClass="text-center">
+                                <p>{this.props.name}</p>
+                            </Row>
+                            <Row>
+                                <Image onLoad={this._hideLoading} id="sharedImgModal" src={source} rounded />
+                                <div id="loading">
+                                    <img id="loader" src="src/loading1.gif"/>
+                                </div>
+                            </Row>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={this._close.bind(this)}>Close</Button>
+                        </Modal.Footer>
+                    </Modal>
                 </div>
             </div>
         );
     }
     componentWillReceiveProps(nextProps){
         this.setState({type:nextProps.type});
+    }
+    _previewImage(){
+        this.setState({showModal: true});
+    }
+    _close(){
+        this.setState({ showModal: false});
     }
     // componentWillMount(){
     //     this.setState({source:source});
