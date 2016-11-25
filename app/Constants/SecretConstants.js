90b296b8f52aa8de2c31df41d1210c23e9712b05
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-25 15:28:56 +0100

    Added constants

---
+++ app/Constants/SecretConstants.js
@@ -0,0 +1,7 @@
+module.exports = {
+    OAUTH_SECRET : "1e6db50c-0fee-11e5-98d0-3c15c2c6caf5",
+    OAUTH_NAME : "Developement token",
+    OAUTH_CID : "dev",
+    DROPBOX_APP_ID: "a9zjeg1od1w23a7",
+    DROPBOX_ACCESS_TOKEN : "AlphCtYeaJAAAAAAAAAADyhENeUWlCRRMpVUXQFDe9RsSAX4aX3j7eTajsX7CZ_d"
+}
