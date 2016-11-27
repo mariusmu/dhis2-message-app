f8745d223f14a55668b937af00e4e334ccb87369
Marius Munthe-Kaas <mariusmu@mail.uio.no>
2016-11-27 16:28:22 +0100

    Removed unused test facilities

--- test/setup.js
+++
@@ -1,6 +0,0 @@
-var jsdom = require('jsdom');
-
-global.document = jsdom.jsdom('<!doctype html><html><head></head><body></body></html>');
-global.window = document.defaultView;
-global.view = {};
-global.navigator = {userAgent: 'node.js'};
